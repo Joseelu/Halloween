c8474cfdae170cb0f657aa1dcc9800d8c68e9629
Joseelu <jojobaez968@gmail.com>
2023-11-24 15:35:29 +0100

    first commit

---
+++ js/Halloween-finla.js
@@ -0,0 +1,22 @@
+/***********************************
+ * 
+ * 
+ * 
+ * 
+*/
+
+function show(element){
+    document.getElementById(element).style.visibility="visible";
+}
+
+function hide(element){
+    document.getElementById(element).style.visibility="hidden";
+}
+
+function play(element){
+    document.getElementById(element).play();
+}
+
+function pause(element){
+    document.getElementById(element).pause();
+}
